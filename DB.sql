a33f61e1bab98d8a9f9b83bbcb7a519d0aef92a0
Muhammad Ammar <57048049+muhamad-ammar@users.noreply.github.com>
2019-12-04 19:10:21 +0500

    Updated Database with FK

--- DB.sql
+++ DB.sql
@@ -1,9 +1,16 @@
 Use hospital;
+Drop table If exists Patient;
+Drop table If exists Doctor;
+Drop table If exists Receptionist;
+Drop table if exists Medicine;
+Drop table if exists Pharmacist;
 Create Table Login(
 role varchar(30) not NULL,
 LoginID varchar(30) Primary Key,
 PIN int not Null
 );
+Drop table Patient;
+Drop table Doctor;
 Create Table Manager(
 Name varchar(40) not Null,
 SID varchar(20) Primary Key,
@@ -17,7 +24,7 @@ Name varchar(30),
 Contact varchar(20),
 Gender enum("Male","Female",'M','F'),
 Qualification varchar(20) not Null,
-Appno varchar(10)
+Appno varchar(10) unique 
 );
 create Table Patient(
 PID varchar(20) Primary Key,
@@ -25,7 +32,7 @@ Name varchar(30),
 Contact varchar(20),
 Gender enum("Male","Female",'M','F','Nil'),
 Disease varchar(20) not Null,
-Appno varchar(10) not Null
+Appno varchar(10) unique
 );
 create Table Receptionist(
 PID varchar(20) not Null,
@@ -42,3 +49,10 @@ Create Table Medicine(
 MedID varchar(10) Primary Key,
 Dose varchar(200) not Null
 );
+
+Alter Table Doctor add Foreign Key (Appno) references Receptionist(Appno);
+Alter Table Patient add Foreign Key (Appno) references Receptionist(Appno);
+Alter Table Pharmacist add Foreign Key (PID) references Patient(PID);
+Alter Table Pharmacist add Foreign Key (DocID) references Doctor(DocID);
+Alter Table Pharmacist add Foreign Key (MedID) references Medicine(MedID);
+
