4760dfbc6d1065b07e42bf46d5df305c5b463d1e
Muhammad Ammar <57048049+muhamad-ammar@users.noreply.github.com>
2019-12-12 16:11:10 +0500

    Updated

--- DB.sql
+++ DB.sql
@@ -1,16 +1,16 @@
-Use hospital;
+Use checkin;
 Drop table If exists Patient;
 Drop table If exists Doctor;
 Drop table If exists Receptionist;
 Drop table if exists Medicine;
 Drop table if exists Pharmacist;
+Drop table if exists Login;
 Create Table Login(
 role varchar(30) not NULL,
 LoginID varchar(30) Primary Key,
 PIN int not Null
 );
-Drop table Patient;
-Drop table Doctor;
+
 Create Table Manager(
 Name varchar(40) not Null,
 SID varchar(20) Primary Key,
@@ -24,7 +24,6 @@ Name varchar(30),
 Contact varchar(20),
 Gender enum("Male","Female",'M','F'),
 Qualification varchar(20) not Null,
-Appno varchar(10) unique,
 LoginId varchar(30) unique
 );
 create Table Patient(
@@ -50,6 +49,7 @@ Create Table Medicine(
 MedID varchar(10) Primary Key,
 Dose varchar(200) not Null
 );
+
 
 Alter Table Doctor add Foreign Key (Appno) references Receptionist(Appno);
 Alter Table Patient add Foreign Key (Appno) references Receptionist(Appno);
